fd3975d0a759bcb5c6dd75f1965b89f5da886d6f
Sergei Maertens <sergei@maykinmedia.nl>
2018-06-08 16:43:33 +0200

    Limit to CREATE and DETAIL actions for now

--- src/zrc/api/viewsets.py
+++ src/zrc/api/viewsets.py
@@ -1,20 +1,14 @@
-from django.utils.decorators import method_decorator
-
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+from zds_schema.decorators import action_description
 
 from zrc.datamodel.models import Zaak, Status
 
 from .serializers import ZaakSerializer, StatusSerializer
 
 
-@method_decorator(name='create', decorator=swagger_auto_schema(
-    operation_description=(
-        "Registreer een ZAAK.\n\nIndien geen zaakidentificatie gegeven is, "
-        "dan wordt deze automatisch gegenereerd."
-    )
-))
-class ZaakViewSet(viewsets.ModelViewSet):
+@action_description('create', "Maak een ZAAK aan.\n\nIndien geen zaakidentificatie gegeven is, "
+                              "dan wordt deze automatisch gegenereerd.")
+class ZaakViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     """
     Opvragen en bewerken van ZAAKen.
     """
@@ -22,6 +16,6 @@ class ZaakViewSet(viewsets.ModelViewSet):
     serializer_class = ZaakSerializer
 
 
-class StatusViewSet(viewsets.ModelViewSet):
+class StatusViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
