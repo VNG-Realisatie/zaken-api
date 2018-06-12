1bfe35f39f76d6a6dba3b21641ae632a3099057f
Sergei Maertens <sergei@maykinmedia.nl>
2018-06-12 10:05:06 +0200

    Expose models in the admin

--- src/zrc/datamodel/admin.py
+++ src/zrc/datamodel/admin.py
@@ -1,14 +1,53 @@
 from django.contrib import admin
 
-from .models import Status, Zaak
+from .models import DomeinData, KlantContact, Status, Zaak, ZaakObject
+
+
+class StatusInline(admin.TabularInline):
+    model = Status
+
+
+class ZaakObjectInline(admin.TabularInline):
+    model = ZaakObject
+
+
+class DomeinDataInline(admin.TabularInline):
+    model = DomeinData
+
+
+class KlantContactInline(admin.TabularInline):
+    model = KlantContact
 
 
 @admin.register(Zaak)
 class ZaakAdmin(admin.ModelAdmin):
     list_display = ['zaakidentificatie']
+    inlines = [StatusInline, ZaakObjectInline, KlantContactInline, DomeinDataInline]
 
 
 @admin.register(Status)
 class StatusAdmin(admin.ModelAdmin):
     list_display = ['zaak', 'datum_status_gezet']
     list_select_related = ['zaak']
+    raw_id_fields = ['zaak']
+
+
+@admin.register(ZaakObject)
+class ZaakObjectAdmin(admin.ModelAdmin):
+    list_display = ['zaak', 'object', 'relatieomschrijving']
+    list_select_related = ['zaak']
+    raw_id_fields = ['zaak']
+
+
+@admin.register(KlantContact)
+class KlantContactAdmin(admin.ModelAdmin):
+    list_display = ['zaak', 'identificatie', 'datumtijd', 'kanaal']
+    list_select_related = ['zaak']
+    raw_id_fields = ['zaak']
+
+
+@admin.register(DomeinData)
+class DomeinDataAdmin(admin.ModelAdmin):
+    list_display = ['zaak', 'domein_data']
+    list_select_related = ['zaak']
+    raw_id_fields = ['zaak']
