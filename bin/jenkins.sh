8e8bd9f8055872c12a336bd9dc15ea0eb7e943bb
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-02 16:14:37 +0200

    Write reports to volume so Jenkins can read it

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -3,5 +3,9 @@
 set -e  # exit on errors
 set -x  # echo commands
 
+if [[ -z "$WORKSPACE" ]]; then
+    export WORKSPACE=$(pwd)
+fi
+
 docker-compose -p zrc_tests -f ./docker-compose.yml build tests
 docker-compose -p zrc_tests -f ./docker-compose.yml run tests
