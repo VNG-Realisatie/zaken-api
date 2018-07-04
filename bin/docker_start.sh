baab9e4213b26fb2fe068ae80d2d62f8b8d054ab
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-04 17:20:03 +0200

    Prepare for continuous integration/deployment

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -26,5 +26,4 @@ python src/manage.py migrate
 
 # Start server
 >&2 echo "Starting server"
-
 uwsgi --http :8000 --module zrc.wsgi --static-map /static=/app/static --chdir=src
--- bin/release-docker-image.sh
+++ bin/release-docker-image.sh
@@ -5,7 +5,17 @@ set -x # echo commands
 
 CONTAINER_REPO=nlxio/gemma-zrc
 
-RELEASE_TAG=${RELEASE_TAG:-latest}
+
+git_tag=$(git tag --points-at HEAD) &>/dev/null
+
+
+if [[ -n "$git_tag" ]]; then
+    echo "Building image for git tag $git_tag"
+    RELEASE_TAG=$git_tag
+else
+    RELEASE_TAG=${RELEASE_TAG:-latest}
+fi
+
 
 docker build \
     --target production \
@@ -15,8 +25,18 @@ docker build \
 
 # JOB_NAME is set by Jenkins
 # only push the image if running in CI
-if [[ ! -z "$JOB_NAME" ]]; then
+if [[ -n "$JOB_NAME" ]]; then
     docker push ${CONTAINER_REPO}:${RELEASE_TAG}
+
+    # if on jenkins AND it's a tagged release -> prepare deployment
+    if [[ -n "$JENKINS_URL" && -n "$git_tag" ]]; then
+        echo "
+VERSION=${git_tag}
+" > deployment-parameters
+    fi
 else
     echo "Not pushing image, set the JOB_NAME envvar to push after building"
 fi
+
+
+# If on Jenkins, write the
