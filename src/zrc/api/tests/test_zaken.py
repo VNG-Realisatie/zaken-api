085e6b94ecd0253f794cf30f7738ca0b24d5be35
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-23 16:56:17 +0200

    Added test for DSO API-44

--- src/zrc/api/tests/test_zaken.py
+++ src/zrc/api/tests/test_zaken.py
@@ -1,7 +1,11 @@
 import unittest
 
+from django.contrib.gis.geos import Point
+
 from rest_framework import status
 from rest_framework.test import APITestCase
+
+from zrc.datamodel.tests.factories import ZaakFactory
 
 from .utils import reverse
 
@@ -19,6 +23,26 @@ class ApiStrategyTests(APITestCase):
     @unittest.expectedFailure
     def test_api_12_subset_fields(self):
         raise NotImplementedError
+
+    def test_api_44_crs_headers(self):
+        # We wijken bewust af - EPSG:4326 is de standaard projectie voor WGS84
+        # De andere opties in de API strategie lijken in de praktijk niet/nauwelijks
+        # gebruikt te worden, en zien er vreemd uit t.o.v. wel courant gebruikte
+        # opties.
+        zaak = ZaakFactory.create(zaakgeometrie=Point(4.887990, 52.377595))  # LONG LAT
+        url = reverse('zaak-detail', kwargs={'pk': zaak.pk})
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_412_PRECONDITION_FAILED)
+
+        response = self.client.get(url, headers={'Accept-Crs': 'dummy'})
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+        response = self.client.get(url, headers={'Accept-Crs': 'EPSG:4326'})
+        self.assertEqual(
+            response['Content-Crs'],
+            'EPSG:4326'
+        )
 
     def test_api_51_status_codes(self):
         with self.subTest(crud='create'):
