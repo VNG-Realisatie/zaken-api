70ab1db6f8a079e3ba6681a8d42a1da2a9100808
Sergei Maertens <sergei@maykinmedia.nl>
2018-07-24 11:04:24 +0200

    Fix concurrent builds by deriving the project name from dir name

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -10,13 +10,8 @@ fi
 # use the Jenkins specific override
 cp bin/docker-compose.override.yml docker-compose.override.yml
 
-docker-compose \
-    -p zrc_tests \
-    build tests
-
-docker-compose \
-    -p zrc_tests \
-    run tests
+docker-compose build tests
+docker-compose run tests
 
 # cleanup
 git reset --hard
