787182e3bbf7e8ea3efc3a985190c96c944ec0ed
Sergei Maertens <sergei@maykinmedia.nl>
2018-08-29 17:35:18 +0200

    Test formaat error messages

--- src/zrc/api/tests/test_dso_api_strategy.py
+++ src/zrc/api/tests/test_dso_api_strategy.py
@@ -1,6 +1,17 @@
-from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework.test import APIRequestFactory, APITestCase
+from rest_framework.views import APIView
 
 from .utils import reverse
+
+
+class NotFoundView(APIView):
+    authentication_classes = ()
+    permission_classes = ()
+
+    def get(self, request, *args, **kwargs):
+        raise NotFound("Some detail message")
 
 
 class DSOApiStrategyTests(APITestCase):
@@ -20,3 +31,22 @@ class DSOApiStrategyTests(APITestCase):
             self.assertGreaterEqual(doc['openapi'], '3.0.0')
         else:
             self.fail('Unknown documentation version')
+
+    def test_api_50_error_handling_standardized(self):
+        view = NotFoundView.as_view()
+        request = APIRequestFactory().get('/some/irrelevant/url', HTTP_ACCEPT='application/json')
+
+        response = view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # can't verify UUID...
+        self.assertTrue(response.data['instance'].startswith('urn:uuid:'))
+        del response.data['instance']
+
+        self.assertEqual(response.data, {
+            'type': "NotFound",
+            'title': "Niet gevonden.",
+            'status': 404,
+            'detail': "Some detail message",
+        })
