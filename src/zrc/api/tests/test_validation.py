790e604b8d4f27d674489ce2b64db73991d7a55e
Sergei Maertens <sergei@maykinmedia.nl>
2018-09-10 16:15:26 +0200

    Ref. vng-Realisatie/gemma-zaken#130 -- regression test for bad invalid-params format

--- src/zrc/api/tests/test_validation.py
+++ src/zrc/api/tests/test_validation.py
@@ -2,6 +2,7 @@ from django.test import override_settings
 
 from rest_framework import status
 from rest_framework.test import APITestCase
+from zds_schema.tests import get_validation_errors
 from zds_schema.validators import URLValidator
 
 from .utils import reverse
@@ -23,7 +24,7 @@ class ZaakValidationTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-        validation_error = response.data['invalid-params'][0]
+        validation_error = get_validation_errors(response, 'zaaktype')
         self.assertEqual(validation_error['code'], URLValidator.code)
         self.assertEqual(validation_error['name'], 'zaaktype')
 
@@ -40,3 +41,16 @@ class ZaakValidationTests(APITestCase):
         }, HTTP_ACCEPT_CRS='EPSG:4326')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_validation_camelcase(self):
+        url = reverse('zaak-list')
+
+        response = self.client.post(url, {}, HTTP_ACCEPT_CRS='EPSG:4326')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        bad_casing = get_validation_errors(response, 'verantwoordelijke_organisatie')
+        self.assertIsNone(bad_casing)
+
+        good_casing = get_validation_errors(response, 'verantwoordelijkeOrganisatie')
+        self.assertIsNotNone(good_casing)
