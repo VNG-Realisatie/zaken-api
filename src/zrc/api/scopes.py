3977ab69818765aa80e01a94a46f64837ddef74b
Sergei Maertens <sergei@maykinmedia.nl>
2018-11-07 14:41:49 +0100

    :memo: Documenteer betekenis van de scopes

--- src/zrc/api/scopes.py
+++ src/zrc/api/scopes.py
@@ -1,7 +1,37 @@
 from zds_schema.scopes import Scope
 
-SCOPE_ZAKEN_CREATE = Scope('zds.scopes.zaken.aanmaken')
+SCOPE_ZAKEN_CREATE = Scope(
+    'zds.scopes.zaken.aanmaken',
+    description="""
+**Laat toe om**:
 
-SCOPE_STATUSSEN_TOEVOEGEN = Scope('zds.scopes.statussen.toevoegen')
+* een zaak aan te maken
+* de eerste status bij een zaak te zetten
+* zaakobjecten aan te maken
+* rollen aan te maken
+"""
+)
 
-SCOPE_ZAKEN_ALLES_LEZEN = Scope('zds.scopes.zaken.lezen')
+SCOPE_STATUSSEN_TOEVOEGEN = Scope(
+    'zds.scopes.statussen.toevoegen',
+    description="""
+**Laat toe om**:
+
+* Statussen toe te voegen voor een zaak
+"""
+)
+
+SCOPE_ZAKEN_ALLES_LEZEN = Scope(
+    'zds.scopes.zaken.lezen',
+    description="""
+**Laat toe om**:
+
+* zaken op te lijsten
+* zaken te doorzoeken
+* zaakdetails op te vragen
+* statussen te lezen
+* statusdetails op te vragen
+* zaakobjecten te lezen
+* zaakobjectdetails op te vragen
+"""
+)
