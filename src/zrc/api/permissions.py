8e1c776b52926f4422229a5bed8ac31a06b5ca2c
annashamray <anna.shamray@maykinmedia.nl>
2019-05-09 16:26:57 +0200

    clean imports

--- src/zrc/api/permissions.py
+++ src/zrc/api/permissions.py
@@ -1,15 +1,11 @@
-from urllib.parse import urlparse
-
 from django.conf import settings
 
 from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.request import Request
-from rest_framework.views import View
 from vng_api_common.permissions import (
     AuthScopesRequired, BaseAuthRequired, MainObjAuthScopesRequired,
     RelatedObjAuthScopesRequired, get_required_scopes
 )
-from vng_api_common.utils import get_resource_for_path
 
 
 def bypass_permissions(request: Request) -> bool:
