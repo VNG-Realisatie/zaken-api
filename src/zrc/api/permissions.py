71b94bbea70aca8299579d1db713823c76f73526
Sergei Maertens <sergei@maykinmedia.nl>
2019-05-13 15:13:07 +0200

    :fire: Remove dead/unused code

--- src/zrc/api/permissions.py
+++ src/zrc/api/permissions.py
@@ -1,17 +1,6 @@
-from django.conf import settings
-
-from rest_framework.renderers import BrowsableAPIRenderer
-from rest_framework.request import Request
 from vng_api_common.permissions import (
     BaseAuthRequired, MainObjAuthScopesRequired, RelatedObjAuthScopesRequired
 )
-
-
-def bypass_permissions(request: Request) -> bool:
-    """
-    Bypass permission checks in DBEUG when using the browsable API renderer
-    """
-    return settings.DEBUG and isinstance(request.accepted_renderer, BrowsableAPIRenderer)
 
 
 class ZaakAuthScopesRequired(MainObjAuthScopesRequired):
@@ -31,6 +20,6 @@ class ZaakRelatedAuthScopesRequired(RelatedObjAuthScopesRequired):
     obj_path = 'zaak'
 
 
-class ZaakBaseAuthRequired (BaseAuthRequired):
+class ZaakBaseAuthRequired(BaseAuthRequired):
     permission_fields = ('zaaktype', 'vertrouwelijkheidaanduiding')
     obj_path = 'zaak'
