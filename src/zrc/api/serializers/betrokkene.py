a420e45755243e9118322f4292db28fac37baac6
annashamray <anna.shamray@maykinmedia.nl>
2019-07-05 16:38:32 +0200

    add SubVerblijfBuitenland to betrokkene-related api

--- src/zrc/api/serializers/betrokkene.py
+++ src/zrc/api/serializers/betrokkene.py
@@ -6,15 +6,28 @@ from vng_api_common.serializers import add_choice_values_help_text
 from zrc.datamodel.constants import GeslachtsAanduiding
 from zrc.datamodel.models import (
     Medewerker, NatuurlijkPersoon, NietNatuurlijkPersoon,
-    OrganisatorischeEenheid, Vestiging
+    OrganisatorischeEenheid, Vestiging, SubVerblijfBuitenland
 )
 from .adres_serializers import VerblijfsAdresSerializer
 
 logger = logging.getLogger(__name__)
 
 
+class SubVerblijfBuitenlandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubVerblijfBuitenland
+        fields = (
+            'lnd_landcode',
+            'lnd_landnaam',
+            'sub_adres_buitenland_1',
+            'sub_adres_buitenland_2',
+            'sub_adres_buitenland_3',
+        )
+
+
 class RolNatuurlijkPersoonSerializer(serializers.ModelSerializer):
     verblijfsadres = VerblijfsAdresSerializer(required=False, allow_null=True)
+    sub_verblijf_buitenland = SubVerblijfBuitenlandSerializer(required=False, allow_null=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -40,15 +53,22 @@ class RolNatuurlijkPersoonSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         verblijfsadres_data = validated_data.pop('verblijfsadres', None)
+        sub_verblijf_buitenland_data = validated_data.pop('sub_verblijf_buitenland', None)
         natuurlijkpersoon = super().create(validated_data)
 
         if verblijfsadres_data:
             verblijfsadres_data['natuurlijkpersoon'] = natuurlijkpersoon
             VerblijfsAdresSerializer().create(verblijfsadres_data)
+
+        if sub_verblijf_buitenland_data:
+            sub_verblijf_buitenland_data['natuurlijkpersoon'] = natuurlijkpersoon
+            SubVerblijfBuitenlandSerializer().create(sub_verblijf_buitenland_data)
         return natuurlijkpersoon
 
 
 class RolNietNatuurlijkPersoonSerializer(serializers.ModelSerializer):
+    sub_verblijf_buitenland = SubVerblijfBuitenlandSerializer(required=False, allow_null=True)
+
     class Meta:
         model = NietNatuurlijkPersoon
         fields = (
@@ -60,9 +80,19 @@ class RolNietNatuurlijkPersoonSerializer(serializers.ModelSerializer):
             'sub_verblijf_buitenland'
         )
 
+    def create(self, validated_data):
+        sub_verblijf_buitenland_data = validated_data.pop('sub_verblijf_buitenland', None)
+        nietnatuurlijkpersoon = super().create(validated_data)
+
+        if sub_verblijf_buitenland_data:
+            sub_verblijf_buitenland_data['nietnatuurlijkpersoon'] = nietnatuurlijkpersoon
+            SubVerblijfBuitenlandSerializer().create(sub_verblijf_buitenland_data)
+        return nietnatuurlijkpersoon
+
 
 class RolVestigingSerializer(serializers.ModelSerializer):
     verblijfsadres = VerblijfsAdresSerializer(required=False, allow_null=True)
+    sub_verblijf_buitenland = SubVerblijfBuitenlandSerializer(required=False, allow_null=True)
 
     class Meta:
         model = Vestiging
@@ -75,12 +105,17 @@ class RolVestigingSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         verblijfsadres_data = validated_data.pop('verblijfsadres', None)
-        natuurlijkpersoon = super().create(validated_data)
+        sub_verblijf_buitenland_data = validated_data.pop('sub_verblijf_buitenland', None)
+        vestiging = super().create(validated_data)
 
         if verblijfsadres_data:
-            verblijfsadres_data['natuurlijkpersoon'] = natuurlijkpersoon
+            verblijfsadres_data['vestiging'] = vestiging
             VerblijfsAdresSerializer().create(verblijfsadres_data)
-        return natuurlijkpersoon
+
+        if sub_verblijf_buitenland_data:
+            sub_verblijf_buitenland_data['vestiging'] = vestiging
+            SubVerblijfBuitenlandSerializer().create(sub_verblijf_buitenland_data)
+        return vestiging
 
 
 class RolOrganisatorischeEenheidSerializer(serializers.ModelSerializer):
