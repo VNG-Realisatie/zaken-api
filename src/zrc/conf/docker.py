a100378862bb20c81d07c7defa4c21ead22ce6b2
Steven Bal <steven@maykinmedia.nl>
2019-08-12 12:20:14 +0200

    Configured redis host for docker settings

--- src/zrc/conf/docker.py
+++ src/zrc/conf/docker.py
@@ -45,7 +45,7 @@ CACHES = {
     # Cache for BIO removal sync with DRC
     'drc_sync': {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2", # NOTE: watch out for multiple projects using the same cache!
+        "LOCATION": f"redis://{getenv('REDIS_CACHE')}", # NOTE: watch out for multiple projects using the same cache!
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "IGNORE_EXCEPTIONS": True,
