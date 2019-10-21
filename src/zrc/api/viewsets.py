d9ee14f2b36cb83eaeb5007e64f4d1fbe268f399
annashamray <47745906+annashamray@users.noreply.github.com>
2019-10-21 11:03:32 +0200

    Apply suggestions from code review
    Co-Authored-By: Joeri Bekker <joeri@maykinmedia.nl>

--- src/zrc/api/viewsets.py
+++ src/zrc/api/viewsets.py
@@ -104,7 +104,7 @@ class ZaakViewSet(
 
     **Er wordt gevalideerd op**:
     - `zaaktype` moet een geldige URL zijn.
-    - `zaaktype` being published (`zaaktype.concept` = False)
+    - `zaaktype` is geen concept (`zaaktype.concept` = False)
     - `laatsteBetaaldatum` mag niet in de toekomst liggen.
     - `laatsteBetaaldatum` mag niet gezet worden als de betalingsindicatie
       "nvt" is.
@@ -135,7 +135,7 @@ class ZaakViewSet(
 
     **Er wordt gevalideerd op**
     - `zaaktype` mag niet gewijzigd worden.
-    - `zaaktype` being published (`zaaktype.concept` = False)
+    - `zaaktype` is geen concept (`zaaktype.concept` = False)
     - `identificatie` mag niet gewijzigd worden.
     - `laatsteBetaaldatum` mag niet in de toekomst liggen.
     - `laatsteBetaaldatum` mag niet gezet worden als de betalingsindicatie
@@ -161,7 +161,7 @@ class ZaakViewSet(
 
     **Er wordt gevalideerd op**
     - `zaaktype` mag niet gewijzigd worden.
-    - `zaaktype` being published (`zaaktype.concept` = False)
+    - `zaaktype` is geen concept (`zaaktype.concept` = False)
     - `identificatie` mag niet gewijzigd worden.
     - `laatsteBetaaldatum` mag niet in de toekomst liggen.
     - `laatsteBetaaldatum` mag niet gezet worden als de betalingsindicatie
