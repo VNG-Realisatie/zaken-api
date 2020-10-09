d1f2a27777d72fd82bae961c7372678d611f4d5e
Steven Bal <steven@maykinmedia.nl>
2020-10-09 16:34:34 +0200

    :bug: Fix incorrect Rol filterparameter name
    issue: https://github.com/VNG-Realisatie/gemma-zaken/issues/1685

--- src/zrc/api/filters.py
+++ src/zrc/api/filters.py
@@ -107,7 +107,7 @@ class RolFilter(FilterSet):
         field_name="vestiging__vestigings_nummer",
         help_text=get_help_text("datamodel.Vestiging", "vestigings_nummer"),
     )
-    betrokkene_identificatie__vestiging__identificatie = filters.CharFilter(
+    betrokkene_identificatie__organisatorische_eenheid__identificatie = filters.CharFilter(
         field_name="organisatorischeeenheid__identificatie",
         help_text=get_help_text("datamodel.OrganisatorischeEenheid", "identificatie"),
     )
@@ -128,7 +128,7 @@ class RolFilter(FilterSet):
             "betrokkene_identificatie__niet_natuurlijk_persoon__inn_nnp_id",
             "betrokkene_identificatie__niet_natuurlijk_persoon__ann_identificatie",
             "betrokkene_identificatie__vestiging__vestigings_nummer",
-            "betrokkene_identificatie__vestiging__identificatie",
+            "betrokkene_identificatie__organisatorische_eenheid__identificatie",
             "betrokkene_identificatie__medewerker__identificatie",
             "roltype",
             "omschrijving",
