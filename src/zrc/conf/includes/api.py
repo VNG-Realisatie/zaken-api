ab23c61f820faee502064d830ba70b150149e371
Steven Bal <steven@maykinmedia.nl>
2020-12-11 11:31:36 +0100

    :wrench: Add necessary settings for autorisaties/notificaties.md generation
    issue: https://github.com/VNG-Realisatie/gemma-zaken/issues/1706

--- src/zrc/conf/includes/api.py
+++ src/zrc/conf/includes/api.py
@@ -1,3 +1,5 @@
+import os
+
 from vng_api_common.conf.api import *  # noqa - imports white-listed
 
 API_VERSION = "1.0.2"
@@ -55,6 +57,10 @@ DRC_API_SPEC = f"https://raw.githubusercontent.com/{drc_repo}/{drc_commit}/src/o
 zrc_repo = "vng-Realisatie/gemma-zaakregistratiecomponent"
 zrc_commit = "8ea1950fe4ec2ad99504d345eba60a175eea3edf"
 ZRC_API_SPEC = f"https://raw.githubusercontent.com/{zrc_repo}/{zrc_commit}/src/openapi.yaml"  # noqa
+
+SELF_REPO = zrc_repo
+SELF_BRANCH = os.getenv("SELF_BRANCH") or API_VERSION
+GITHUB_API_SPEC = f"https://raw.githubusercontent.com/{SELF_REPO}/{SELF_BRANCH}/src/openapi.yaml"  # noqa
 
 SPEC_CACHE_TIMEOUT = 60 * 60 * 24  # 24 hours
 
