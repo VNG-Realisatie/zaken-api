307399bd07c6b8a7237fd43c59d907a87457075d
Sonny Bakker <sonny@maykinmedia.nl>
2022-09-28 11:30:31 +0200

    Update schema

--- src/zrc/api/__init__.py
+++ src/zrc/api/__init__.py
@@ -8,6 +8,7 @@ from vng_api_common.extensions.fields.hyperlink_identity import (
 from vng_api_common.extensions.fields.hyperlinked_related import (
     HyperlinkedRelatedFieldExtension,
 )
+from vng_api_common.extensions.fields.many_related import ManyRelatedFieldExtension
 from vng_api_common.extensions.fields.read_only import ReadOnlyFieldExtension
 from vng_api_common.extensions.filters.query import FilterExtension
 from vng_api_common.extensions.serializers.gegevensgroep import GegevensGroepExtension
