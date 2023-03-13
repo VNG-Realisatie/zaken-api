774ab213ec7a7d11f52431ece06411713a3611a8
joerivrij <joerivrijaldenhoven@gmail.com>
2023-03-13 08:35:30 +0100

    feat: adds notification env flag (#258)

--- src/zrc/conf/docker.py
+++ src/zrc/conf/docker.py
@@ -7,8 +7,22 @@ os.environ.setdefault("DB_PASSWORD", "")
 
 from .production import *  # noqa isort:skip
 
+# Helper function
+missing_environment_vars = []
+
+
+def getenv(key, default=None, required=False, split=False):
+    val = os.getenv(key, default)
+    if required and val is None:
+        missing_environment_vars.append(key)
+    if split and val:
+        val = val.split(",")
+    return val
+
+
 #
 # Custom settings
 #
 
 ENVIRONMENT = "docker"
+NOTIFICATIONS_DISABLED = bool(getenv("NOTIFICATIONS_DISABLED", False))
